4d86b89b21734ca966cda20ea08e20a60524dbbf
goingtop <sebasipi501@gmail.com>
2018-08-23 17:16:19 -0500

    AbsolutoLayout

--- DemoStackLayout/App.xaml.cs
+++ DemoStackLayout/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DemoStackLayout
         {
             InitializeComponent();
 
-            MainPage = new Ejercicio();
+            MainPage = new AbsolutoLayout();
         }
 
         protected override void OnStart()
