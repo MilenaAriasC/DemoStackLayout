8fb49900fd454b85dd7bab8b8b89057d085c8a6f
goingtop <sebasipi501@gmail.com>
2018-08-23 17:01:20 -0500

    ejercicio

--- DemoStackLayout/App.xaml.cs
+++ DemoStackLayout/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DemoStackLayout
         {
             InitializeComponent();
 
-            MainPage = new Expancion();
+            MainPage = new Ejercicio();
         }
 
         protected override void OnStart()
